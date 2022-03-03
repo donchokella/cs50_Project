81a1f75b95c8a9c0ee52d71591aeceed951936c6
donchokella <ali.alparslan@outlook.com>
2022-03-03 11:48:19 +0300

    Brick Breaker is initialized

---
+++ main.lua
@@ -0,0 +1,16 @@
+--[[
+This is a Brick Breaker game remake
+
+Author: Ali Alparslan
+ali.alparslan@outlook.com
+
+Originally programmed as "Breakout" by Atari in 1976.
+
+There are one paddle, one ball and lots of bricks.
+Paddle is placed on the bottom of the screen, bricks are stacks at the top, and the ball is moving between ball and the bricks.
+The goal it to break all the bricks
+
+There is a paddle, a ball and a lot of bricks.
+The paddle is at the bottom of the screen, the bricks are stacked on top, and the ball moves between the bricks and the paddle.
+The paddle is under the player's control. The player has 3 lives. The goal is to break all the bricks. While doing this, the ball must not pass behind the paddle. If this happens, the player loses one life.
+]]
