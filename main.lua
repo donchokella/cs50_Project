8ebc36f0e2fc69e364a226adcc7da69ac74fd4e0
donchokella <ali.alparslan@outlook.com>
2022-03-03 19:45:25 +0300

    Tis is Not a Brick Breaker Game

--- main.lua
+++ main.lua
@@ -1,27 +1,36 @@
 --[[
-This is a Brick Breaker game remake
+This is Not a Brick Breaker Game
 
 Author: Ali Alparslan
 ali.alparslan@outlook.com
 
-Originally programmed as "Breakout" by Atari in 1976.
-
-There are one paddle, one ball and lots of bricks.
-Paddle is placed on the bottom of the screen, bricks are stacks at the top, and the ball is moving between ball and the bricks.
-The goal it to break all the bricks
-
-There is a paddle, a ball and a lot of bricks.
+Brick Breaker originally programmed as "Breakout" by Atari in 1976.
+There is a paddle, a ball, and a lot of bricks.
 The paddle is at the bottom of the screen, the bricks are stacked on top, and the ball moves between the bricks and the paddle.
-The paddle is under the player's control. The player has 3 lives. The goal is to break all the bricks. While doing this, the ball must not pass behind the paddle. If this happens, the player loses one life.
+The paddle is under the player's control. The player has 3 lives. The goal is to break all the bricks. 
+While doing this, the ball must not pass behind the paddle. If this happens, the player loses one life.
+
+However...
+
+as i said, this is 'Not a Brick Breaker Game'. In this game, you cannot even see a tiny piece of brick. 
+The only goal in this game is to get the highest score without missing the ball behind the racket. 
+You can track how many times you hit the ball from the top left corner. Remember, the ball is getting faster and faster!
 ]]
 
 function love.load()
-    love.window.setTitle('A Brick Breaker Game by donchokella') --the title of the window
+    love.window.setTitle('This is Not a Brick Breaker Game') --the title of the window
     math.randomseed(os.time())  --it is important for absoulute randomness
+
+    sounds = {
+        ['paddle'] = love.audio.newSource('sounds/Paddle.wav', 'static'),
+        ['wall'] = love.audio.newSource('sounds/Wall.wav', 'static'),
+        ['gameover'] = love.audio.newSource('sounds/Gameover.wav', 'static')
+    }
 
     windows = {}
     windows.width = 800
     windows.height = 600
+    windows.base = 530
 
     paddle = {}
     paddle.x = 400
@@ -33,12 +42,14 @@ function love.load()
     ball = {}
     ball.x = 400
     ball.y = 300
-    ball.radius = 5
-    ball.speed = 5
-    ball.speedx = math.random(2) == 1 and -100 or 100                                    --here will change
-    ball.speedy = math.random(2) == 1 and math.random(-80, -100) or math.random(80, 100) --here will change
-    ball.width = ball.radius * 2
-    ball.height = ball. radius * 2
+    ball.radius = 4
+    ball.speedx = (math.random(2) == 1 and -100 or 100)*2                                   --here will change
+    ball.speedy = (math.random(2) == 1 and math.random(-80, -100) or math.random(80, 100))*2 --here will change
+    
+    hit_timer = 0
+    increment = 1.05
+
+    state = 'pause'
 end
 
 function love.update(dt)
@@ -58,39 +69,61 @@ function love.update(dt)
     end
 
     -- Ball movement
-    ball.x = ball.x + ball.speedx*dt
-    ball.y = ball.y + ball.speedy*dt
-
-    if ball.x > windows.width or ball.x < 0 then
-        ball.speed = ball.speed * -1
+    if state == 'play' then
+        ball.x = ball.x + ball.speedx*dt
+        ball.y = ball.y + ball.speedy*dt
+    else
+        ball.x = ball.x
+        ball.y = ball.y
     end
 
     --Ball and Paddle Collision
     if coll_ball_paddle() == true then
-        ball.speedy = ball.speedy * -1
+        ball.speedy = ball.speedy * -1 * increment
+        sounds['paddle']:play()
+        hit_timer = hit_timer + 1
     end
 
-    --Ball and Wall Collision
+    -- Ball and Wall Collision
     if coll_ball_wally() == true then
-        ball.speedy = ball.speedy * -1
+        ball.speedy = ball.speedy * -1 * increment
+        sounds['wall']:play()
     end
     
     if coll_ball_wallx() == true then
-        ball.speedx = ball.speedx * -1
+        ball.speedx = ball.speedx * -1 * increment
+        sounds['wall']:play()
+    end
+    -- Finish State
+    if ball.y > windows.base + 2 then
+        ball.x = 400
+        ball.y = 300
+        sounds['gameover']:play()
+        state = 'finish'
     end
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if state == 'pause' then
+            state = 'play'
+        elseif state == 'play' then
+            state = 'pause'
+        elseif state == 'finish' then
+            state = 'pause'
+            hit_timer = 0
+            ball.speedx = (math.random(2) == 1 and -100 or 100)*2                                   
+            ball.speedy = (math.random(2) == 1 and math.random(-80, -100) or math.random(80, 100))*2
+        end
     end
-    
 end
 
 function coll_ball_paddle()
-    if ball.y + ball.height < paddle.y then
+    if ball.y + ball.radius < paddle.y then
         return false
-    elseif ball.x > paddle.x + paddle.width or paddle.x > ball.x + ball.width then
+    elseif ball.x > paddle.x + paddle.width or paddle.x > ball.x + ball.radius then
         return false
     else
         return true
@@ -98,7 +131,7 @@ function coll_ball_paddle()
 end
 
 function coll_ball_wally()
-    if ball.y > 0 then
+    if ball.y - ball.radius > 0 then
         return false
     else
         return true
@@ -106,7 +139,7 @@ function coll_ball_wally()
 end
 
 function coll_ball_wallx()
-    if ball.x > 0 and ball.x < windows.width then
+    if ball.x - ball.radius > 0 and ball.x + ball.radius < windows.width then
         return false
     else
         return true
@@ -115,7 +148,10 @@ end
 
 function love.draw()
     love.graphics.clear(40/255, 200/255, 180/255) --clear the screen with a color
-    
+
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("You hit " .. hit_timer .. " times")
+
     -- Paddle
     love.graphics.setColor(40/255, 40/255, 40/255)
     love.graphics.rectangle('fill', paddle.x, paddle.y, paddle.width, paddle.height)
@@ -123,4 +159,15 @@ function love.draw()
     -- Ball
     love.graphics.setColor(1, 0, 0, 0.8)
     love.graphics.circle('fill', ball.x, ball.y, ball.radius)
+
+    if state == 'pause' then
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.printf('This game is created by donchokella for cs50 Term Project', 0, 20, windows.width, 'center')
+        love.graphics.printf('Press Enter to Begin', 0, 40, windows.width, 'center')
+
+    elseif state == 'play' then
+        -- no UI messages
+    elseif state == 'finish' then
+        love.graphics.printf('Your score is ' .. hit_timer, 0, 100, windows.width, 'center')
+    end
 end
