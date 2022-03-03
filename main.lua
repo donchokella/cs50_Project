e9fa75824d5599cf6bd0e9f21cbfe8990c27f778
donchokella <ali.alparslan@outlook.com>
2022-03-03 12:37:04 +0300

    Paddle is created

--- main.lua
+++ main.lua
@@ -14,3 +14,44 @@ There is a paddle, a ball and a lot of bricks.
 The paddle is at the bottom of the screen, the bricks are stacked on top, and the ball moves between the bricks and the paddle.
 The paddle is under the player's control. The player has 3 lives. The goal is to break all the bricks. While doing this, the ball must not pass behind the paddle. If this happens, the player loses one life.
 ]]
+
+function love.load()
+    love.window.setTitle('A Brick Breaker Game by donchokella') --the title of the window
+    paddle = {}
+    paddle.x = 400
+    paddle. y = 530
+    paddle.width = 50
+    paddle.height = 12
+    paddle.speed = 5
+
+
+end
+
+function love.update()
+
+    -- Paddle movement
+    if love.keyboard.isDown('left') then
+        paddle.x = paddle.x - paddle.speed
+    end
+    if love.keyboard.isDown('right') then
+        paddle.x = paddle.x + paddle.speed
+    end
+    
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+    
+end
+
+
+function love.draw()
+    love.graphics.clear(40/255, 200/255, 180/255) --clear the screen with a color
+
+    love.graphics.rectangle('fill', paddle.x, paddle.y, paddle.width, paddle.height)
+    love.graphics.setColor(40/255, 40/255, 40/255)
+    
+
+end
